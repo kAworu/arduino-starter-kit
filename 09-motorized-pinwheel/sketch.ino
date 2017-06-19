ad5582a0c3b49f4717d510662ca3a706d1368f99
Alexandre Perrin <alex@kaworu.ch>
2017-06-19 23:17:14 +0200

    Add a Potentiometer

--- 09-motorized-pinwheel/sketch.ino
+++ 09-motorized-pinwheel/sketch.ino
@@ -1,11 +1,24 @@
 #define	SWITCHPIN	2
 #define	MOTORPIN	9
+#define	PMPIN		A0
+
 void setup() {
+	Serial.begin(9600);
 	pinMode(SWITCHPIN, INPUT);
 	pinMode(MOTORPIN,  OUTPUT);
 }
 
 void loop() {
 	int state = digitalRead(SWITCHPIN);
-	digitalWrite(MOTORPIN, state);
+	int rate  = analogRead(PMPIN);
+	Serial.print("state=");
+	Serial.print(state == HIGH ? "HIGH" : "LOW");
+	Serial.print(", rate=");
+	Serial.println(rate);
+	// controlled by the switch button
+	//digitalWrite(MOTORPIN, state);
+	// controlled by the Potentiometer
+	// NOTE: rate is [0,1023] and analogWrite expect [0,255] hence we divide
+	// by 4.
+	analogWrite(MOTORPIN, rate / 4);
 }
